090a5acf3e01420e994189ac1913f1a6207d9f01
adyd94 <ady.drozman2007@gmail.com>
2017-11-09 17:44:07 +0200

    test commit 2

---
+++ src/main/java/models/FooterModel.java
@@ -0,0 +1,197 @@
+package models;
+
+public class FooterModel {
+    private String Company;
+    private String Team;
+    private String Careers;
+    private String Blog;
+    private String PressReleases;
+    private String ContactUs;
+    private String ForFamilies;
+    private String ForSchools;
+    private String ForBusiness;
+    private String Resellers;
+    private String Partners;
+    private String Affiliates;
+    private String Help;
+    private String SystemRequirements;
+    private String Resourcesforparents;
+    private String Sitemap;
+    private String Facebook;
+    private String Linkedin;
+    private String Twitter;
+    private String TermsOfService;
+    private String PrivacyPolicy;
+
+
+    public String getCompany() {
+        return Company;
+    }
+
+    public void setCompany(String company) {
+        Company = company;
+    }
+
+    public String getTeam() {
+        return Team;
+    }
+
+    public void setTeam(String team) {
+        Team = team;
+    }
+
+    public String getCareers() {
+        return Careers;
+    }
+
+    public void setCareers(String careers) {
+        Careers = careers;
+    }
+
+    public String getBlog() {
+        return Blog;
+    }
+
+    public void setBlog(String blog) {
+        Blog = blog;
+    }
+
+    public String getPressReleases() {
+        return PressReleases;
+    }
+
+    public void setPressReleases(String pressReleases) {
+        PressReleases = pressReleases;
+    }
+
+    public String getContactUs() {
+        return ContactUs;
+    }
+
+    public void setContactUs(String contactUs) {
+        ContactUs = contactUs;
+    }
+
+    public String getForFamilies() {
+        return ForFamilies;
+    }
+
+    public void setForFamilies(String forFamilies) {
+        ForFamilies = forFamilies;
+    }
+
+    public String getForSchools() {
+        return ForSchools;
+    }
+
+    public void setForSchools(String forSchools) {
+        ForSchools = forSchools;
+    }
+
+    public String getForBusiness() {
+        return ForBusiness;
+    }
+
+    public void setForBusiness(String forBusiness) {
+        ForBusiness = forBusiness;
+    }
+
+    public String getResellers() {
+        return Resellers;
+    }
+
+    public void setResellers(String resellers) {
+        Resellers = resellers;
+    }
+
+    public String getPartners() {
+        return Partners;
+    }
+
+    public void setPartners(String partners) {
+        Partners = partners;
+    }
+
+    public String getAffiliates() {
+        return Affiliates;
+    }
+
+    public void setAffiliates(String affiliates) {
+        Affiliates = affiliates;
+    }
+
+    public String getHelp() {
+        return Help;
+    }
+
+    public void setHelp(String help) {
+        Help = help;
+    }
+
+    public String getSystemRequirements() {
+        return SystemRequirements;
+    }
+
+    public void setSystemRequirements(String systemRequirements) {
+        SystemRequirements = systemRequirements;
+    }
+
+    public String getResourcesforparents() {
+        return Resourcesforparents;
+    }
+
+    public void setResourcesforparents(String resourcesforparents) {
+        Resourcesforparents = resourcesforparents;
+    }
+
+    public String getSitemap() {
+        return Sitemap;
+    }
+
+    public void setSitemap(String sitemap) {
+        Sitemap = sitemap;
+    }
+
+    public String getFacebook() {
+        return Facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        Facebook = facebook;
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        Linkedin = linkedin;
+    }
+
+    public String getTwitter() {
+        return Twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        Twitter = twitter;
+    }
+
+    public String getTermsOfService() {
+        return TermsOfService;
+    }
+
+    public void setTermsOfService(String termsOfService) {
+        TermsOfService = termsOfService;
+    }
+
+    public String getPrivacyPolicy() {
+        return PrivacyPolicy;
+    }
+
+    public void setPrivacyPolicy(String privacyPolicy) {
+        PrivacyPolicy = privacyPolicy;
+    }
+
+
+}
+
