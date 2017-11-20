3866f61d222c21a318e04aada0e8d3b44d6e54b2
AdrianD94 <ady.drozman2007@gmail.com>
2017-11-21 00:39:53 +0200

    DashBoardTopBar v1

---
+++ src/main/java/pages/DashBoardTopBarPage.java
@@ -0,0 +1,146 @@
+package pages;
+
+import models.LoginModel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DashBoardTopBarPage {
+    WebDriver driver;
+
+    public DashBoardTopBarPage(WebDriver driverLn) {
+        {
+            this.driver = driverLn;
+        }
+    }
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement enTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement esTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement frTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement ptTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement itTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement deTranslation;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div")
+    private WebElement TopBar;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"login_form\"]/ul/li[1]/input")
+    private WebElement emailField;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"login_form\"]/ul/li[2]/input")
+    private WebElement passwordField;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"login_button\"]/span")
+    private WebElement loginButton;
+
+    @FindBy (how = How.XPATH, using = "//*[@id=\"LoginFailed\"]/div")
+    private WebElement loginError;
+
+    @FindBy (how = How.XPATH, using = "//*[@id=\"cboxClose\"]")
+    private WebElement closeBaner;
+
+    @FindBy (how = How.XPATH, using = " //*[@id=\"new_header\"]/div/div[2]/div[1]/div[1]/div/div")
+    private WebElement profileButton;
+
+    @FindBy (how = How.XPATH, using = " //*[@id=\"new_header\"]/div/div[2]/div[1]/div[1]/ul/li[1]/a")
+    private WebElement settingsButton;
+
+    @FindBy (how = How.XPATH, using = " //*[@id=\"accountInfo-form\"]/p")
+    private WebElement yourAccountButton;
+
+    @FindBy (how = How.XPATH, using = "  //*[@id=\"selMQQ_chzn\"]/a/span")
+    private WebElement language;
+
+    @FindBy (how = How.XPATH, using = "//*[@id=\"accountInfo-form\"]/div[3]/button[1]")
+    private WebElement saveButton;
+
+    @FindBy (how = How.XPATH, using = "//*[@id=\"selXIT_chzn\"]/a")
+    private WebElement clickSelect;
+
+
+
+    public void LoginFlow() throws InterruptedException {
+        driver.get("http://fp-pre.qustodio.com");
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys("adytestare+pre1711@gmail.com");
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys("test12345");
+        loginButton.click();
+    }
+
+    public String getEnTranslationText(){
+        return this.enTranslation.getText();
+    }
+
+    public String getEsTranslationText(){
+        return this.esTranslation.getText();
+    }
+
+    public String getFrTranslationText(){
+        return this.frTranslation.getText();
+    }
+
+    public String getPtTranslationText(){
+        return this.ptTranslation.getText();
+    }
+
+    public String getItTranslationText(){
+        return this.itTranslation.getText();
+    }
+
+    public String getDeTranslationText(){
+        return this.deTranslation.getText();
+    }
+
+    public void clickTopBar(){
+        this.TopBar.click();
+    }
+
+    public boolean isTopBarVisible(){
+        return TopBar.isDisplayed();
+    }
+
+    public void closeBanner(){
+        this.closeBaner.click();
+    }
+
+    public void clickSelect(){
+        this.clickSelect.click();
+    }
+
+    public void ChangeLanguage(int index) throws InterruptedException{
+        Thread.sleep(5000);
+        profileButton.click();
+        settingsButton.click();
+        yourAccountButton.click();
+        Thread.sleep(5000);
+
+        driver.switchTo().frame(driver.findElement(By.xpath("/html/body/div[2]/iframe")));
+        //clickSelect.click();
+        Select dropdown = new Select(driver.findElement(By.xpath("//*[@id=\"selXIT\"]")));
+        dropdown.deselectAll();
+        dropdown.selectByIndex(index);
+        saveButton.click();
+
+    }
+
+
+}
---
+++ src/main/java/tests/DashBoardTopBarTest.java
@@ -0,0 +1,109 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.TopBarModel;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.DashBoardTopBarPage;
+import pages.TopBarPage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DashBoardTopBarTest extends BaseTestPublicSite {
+
+    @DataProvider(name = "TranslationJson")
+    public Iterator<Object[]> jsonTranslationDataProvider() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Collection<Object[]> dp = new ArrayList<>();
+
+        File[] files = getListOfFiles("topBarTranslate");
+        for (File f : files) {
+            TopBarModel m = objectMapper.readValue(f, TopBarModel.class);
+            dp.add(new Object[]{m});
+        }
+        return dp.iterator();
+    }
+
+    @Test(dataProvider = "TranslationJson")
+    public void topBarTranslationTest(TopBarModel topBarTranslationModel) throws InterruptedException {
+        DashBoardTopBarPage dashboardtopBarPage = PageFactory.initElements(driver, DashBoardTopBarPage.class);
+        Thread.sleep(1000);
+
+
+        dashboardtopBarPage.LoginFlow();
+        Thread.sleep(5000);
+        dashboardtopBarPage.closeBanner();
+
+        try {
+            //Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        }catch (NoSuchElementException e){
+            System.out.println("No top bar");
+        }
+        String EnTopBarTranslation= dashboardtopBarPage.getEnTranslationText();
+        try {
+            Assert.assertEquals( topBarTranslationModel.getEnTranslate(),EnTopBarTranslation);
+        }catch(AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getEnTranslate()+"\nActual: "+EnTopBarTranslation);
+        }
+        //dashboardtopBarPage.clickTopBar();
+        //Assert.assertEquals(driver.getCurrentUrl(), "https://pre.qustodio.com/en/family/premium/" );
+
+        dashboardtopBarPage.ChangeLanguage(2);
+        Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        String EsTopBarTranslation= dashboardtopBarPage.getEsTranslationText();
+        try {
+            Assert.assertEquals(topBarTranslationModel.getEsTranslate(),EsTopBarTranslation);
+        }catch(AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getEsTranslate()+"\nActual: "+EsTopBarTranslation);
+        }
+
+
+
+        Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        String FrTopBarTranslation= dashboardtopBarPage.getFrTranslationText();
+        try {
+            Assert.assertEquals(topBarTranslationModel.getFrTranslate(),FrTopBarTranslation);
+        }catch (AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getFrTranslate()+"\nActual: "+FrTopBarTranslation);
+        }
+
+
+        Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        String ItTopBarTranslation= dashboardtopBarPage.getItTranslationText();
+        try {
+            Assert.assertEquals(topBarTranslationModel.getItTranslate(),ItTopBarTranslation);
+        }catch(AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getItTranslate()+"\nActual: "+ItTopBarTranslation);
+        }
+
+
+
+        Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        String PtTopBarTranslation= dashboardtopBarPage.getPtTranslationText();
+        try {
+            Assert.assertEquals(topBarTranslationModel.getPtTranslate(),PtTopBarTranslation);
+        }catch (AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getPtTranslate()+"\nActual: "+PtTopBarTranslation);
+        }
+
+
+
+        Assert.assertTrue(dashboardtopBarPage.isTopBarVisible());
+        String DeTopBarTranslation= dashboardtopBarPage.getDeTranslationText();
+        try {
+            Assert.assertEquals(topBarTranslationModel.getDeTranslate(),DeTopBarTranslation);
+        }catch (AssertionError e){
+            System.out.println("Expected: " +topBarTranslationModel.getDeTranslate()+"\nActual: "+DeTopBarTranslation);
+        }
+
+
+    }
+
+}
